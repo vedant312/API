e4dac8c2221b391492d5ea8686a7fb6502075be0
vedant312 <vedantd20@iitk.ac.in>
2023-12-18 16:00:20 +0530

    Add express-compression and express-rate-limit dependencies

--- src/server.js
+++ src/server.js
@@ -1,16 +1,24 @@
 import express from 'express';
-import helmet from "helmet"; 
+import helmet from 'helmet';
+import { rateLimit } from 'express-rate-limit';
+import compression from 'compression';
 
 import mainRoute from './main.routes';
 import userRoutes from './user.routes';
 
 const app = express();
 const port = 3000;
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  limit: 12, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+});
 
+app.use(compression())
+app.use(limiter);
 app.use(express.json());
 app.use(helmet());
 
-app.get('/v1',mainRoute);
+app.get('/v1', mainRoute);
 app.use('/v1/user', userRoutes);
 
 app.listen(port, () => {
