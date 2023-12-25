2896ed7bf2ccd4e442ddc0aae92e57050f1e69da
vedant312 <vedantd20@iitk.ac.in>
2023-12-25 18:50:16 +0530

    Fixed bug of repeating user id - after remove then creating

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,3 @@
-import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 import userService from '../services/user.service';
--- src/models/persistence/user.dao.js
+++ src/models/persistence/user.dao.js
@@ -1,55 +1,57 @@
 import users from '../data/users.data';
 
-const getAll = () =>  users;
+const getAll = () => users;
 
 const get = (userId) => users.find((user) => user.id === userId);
 
 const update = (userId, newDetails) => {
-    let existingUser = null;
-    let userIndex;
-    users.map((user, index) => {
-        if (user.id === userId) {
-            existingUser = user;
-            userIndex = index;
-        }
-    });
-
-    if(!existingUser){
-        return null;
+  let existingUser = null;
+  let userIndex;
+  users.map((user, index) => {
+    if (user.id === userId) {
+      existingUser = user;
+      userIndex = index;
     }
+  });
 
-    const updatedUser =  {
-        ...existingUser,
-        ...newDetails
-    }
+  if (!existingUser) {
+    return null;
+  }
 
-    users.splice(userIndex, 1,updatedUser);
+  const updatedUser = {
+    ...existingUser,
+    ...newDetails,
+  };
 
-    return users[userIndex];
-}
+  users.splice(userIndex, 1, updatedUser);
+
+  return users[userIndex];
+};
 
 const insert = (details) => {
-    const newUser = { id: users.length + 1 , ...details };  
-    users.push(newUser);
+  const maxId = users.reduce((max, user) => (user.id > max ? user.id : max), 0);
+  const newUser = { id: maxId + 1, ...details };
+  users.push(newUser);
 
-    return newUser;
-}
+  return newUser;
+};
 
 const remove = (userId) => {
-    const deleteUser = users.find((user, index) => {
-        if (user.id === userId) {
-            users.splice(index, 1);
-            return true;
-        }
-        return false;
-    });
-    return deleteUser;
-}
+  const deleteUser = users.find((user, index) => {
+    if (user.id === userId) {
+      users.splice(index, 1);
+      return true;
+    }
+    return false;
+  });
+
+  return deleteUser;
+};
 
 export default {
-    get,
-    getAll,
-    update,
-    insert,
-    remove
+  get,
+  getAll,
+  update,
+  insert,
+  remove,
 };
